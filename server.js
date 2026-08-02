3e4d2aa6287edd30be27ffd48c8cf7377b72c0d1
evrenfm4-hub <evrenfm4@gmail.com>
2026-08-02 23:14:54 +0200

    Improve match finding and implement chat messaging
    Refactor match finding logic and add chat messaging feature.

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ let waitingPlayer = null;
 io.on('connection', (socket) => {
     console.log('Bir kullanıcı bağlandı:', socket.id);
 
+    // Eşleşme veya oda bulma isteği
     socket.on('find-match', () => {
-        if (waitingPlayer) {
-            // Eşleşme bulundu
+        if (waitingPlayer && waitingPlayer.id !== socket.id) {
             const room = 'room_' + socket.id;
             socket.join(room);
             waitingPlayer.join(room);
@@ -25,6 +25,17 @@ io.on('connection', (socket) => {
         } else {
             waitingPlayer = socket;
             socket.emit('waiting', 'Rakip aranıyor...');
+        }
+    });
+
+    // Oda içi mesajlaşma sistemi
+    socket.on('chat-message', (data) => {
+        // data içinde { roomId, message, sender } gibi bilgiler gelebilir
+        if (data && data.roomId) {
+            io.to(data.roomId).emit('chat-message', data);
+        } else {
+            // Eğer oda belirtilmediyse genel yayınla veya odalara dağıt
+            socket.broadcast.emit('chat-message', data);
         }
     });
 
